49e54d1a743563ccb35479a756c1760dcad5d923
lukemoore495 <lukemoore495@gmail.com>
2022-02-26 16:15:01 -0500

    Fix the line drawings a bit. Also repostiion children to be centered (not accounting for collisions)

--- frontend/src/app/hierarchical-view/hierarchical-view.component.ts
+++ frontend/src/app/hierarchical-view/hierarchical-view.component.ts
@@ -27,24 +27,23 @@ export class HierarchicalViewComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
-        // let currentParent: string | null = null;
-        // let currentChildren: string[] = [];
-        // console.log(this.relationships);
-        // for (let i = 0; i < this.relationships.length; i++) {
-        //     let relationship = this.relationships[i].split(",");
-        //     if (currentParent === null) {
-        //         currentParent = relationship[0];
-        //         currentChildren.push(relationship[1])
-        //     } else if (currentParent !== relationship[0]) {
-        //         this.repositionChildren(currentChildren, currentParent, Number(currentChildren[0][0])+1);
-        //         currentParent = relationship[0];
-        //         currentChildren = [relationship[1]];
-        //     } else {
-        //         currentChildren.push(relationship[1]);
-        //     }
-        // }
-        // if (currentParent !== null)
-        //     this.repositionChildren(currentChildren, currentParent, Number(currentChildren[0][0])+1);
+        let currentParent: string | null = null;
+        let currentChildren: string[] = [];
+        for (let i = 0; i < this.relationships.length; i++) {
+            let relationship = this.relationships[i].split(",");
+            if (currentParent === null) {
+                currentParent = relationship[0];
+                currentChildren.push(relationship[1])
+            } else if (currentParent !== relationship[0]) {
+                this.repositionChildren(currentChildren, currentParent, Number(currentChildren[0][0])+1);
+                currentParent = relationship[0];
+                currentChildren = [relationship[1]];
+            } else {
+                currentChildren.push(relationship[1]);
+            }
+        }
+        if (currentParent !== null)
+            this.repositionChildren(currentChildren, currentParent, Number(currentChildren[0][0])+1);
 
         for (let i = 0; i < this.relationships.length; i++) {
             let relationship = this.relationships[i].split(",");
@@ -57,96 +56,52 @@ export class HierarchicalViewComponent implements OnInit, AfterViewInit {
     }
 
     repositionChildren(childrenIds: string[], parentId: string, level: number) {
-        // let parentPosition = document.getElementById(parentId)?.getBoundingClientRect();
-        // if (!parentPosition) {
-        //     return;
-        // }
+        let parentPosition = document.getElementById(parentId)?.getBoundingClientRect();
+        if (!parentPosition) {
+            return;
+        }
 
-        // const commonOffset = 1000 * (1/(level*2));
-        // if(childrenIds.length % 2 === 0){
-        //     for(let i = 0; i < childrenIds.length; i++){
-        //         let child = document.getElementById(childrenIds[i]);
-        //         if(!child) {
-        //             continue;
-        //         }
-        //         let childPosition = child.getBoundingClientRect();
-        //         let center = parentPosition.x - childPosition.x;
-        //         if(i < childrenIds.length/2){
-        //             let offset = "left:" + (center - (commonOffset/2 * (childrenIds.length/2 - i))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         } else {
-        //             let offset = "left:" + (center + (commonOffset/2 * ((i+1) - childrenIds.length/2))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         }
-        //     }
-        // } else {
-        //     for(let i = 0; i < childrenIds.length; i++){
-        //         let child = document.getElementById(childrenIds[i]);
-        //         if(!child) {
-        //             continue;
-        //         }
-        //         let childPosition = child.getBoundingClientRect();
-        //         let center = parentPosition.x - childPosition.x;
-        //         let middleIndex = ~~(childrenIds.length/2);
-        //         if(i === middleIndex){
-        //             let offset = "left:" + center.toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         }else if(i < middleIndex){
-        //             let offset = "left:" + (center - (commonOffset * (middleIndex - i))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         } else {
-        //             let offset = "left:" + (center + (commonOffset * (i - middleIndex))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         }
-        //     }
-        // }
+        let offset = "";
+        if(childrenIds.length % 2 === 0){
+            let middleIndex1 = childrenIds.length/2;
+            let middleIndex2 = middleIndex1 -1;
+            let middleChild1 = document.getElementById(childrenIds[middleIndex1]);
+            let middleChild1Position = middleChild1?.getBoundingClientRect();
+            let middleChild2 = document.getElementById(childrenIds[middleIndex2]);
+            let middleChild2Position = middleChild2?.getBoundingClientRect();
+            
+            if(!middleChild1 || !middleChild1Position || !middleChild2 || !middleChild2Position)
+                return;
 
+            let middleChild1X = (middleChild1Position.left - middleChild1Position.height + middleChild1Position.width/5);
+            let middleChild2X = (middleChild2Position.left - middleChild2Position.height + middleChild2Position.width/5);
+            let centerPoint = middleChild2X + (middleChild1X - middleChild2X)/2;
+            console.log(middleChild1X, middleChild2X, centerPoint);
+            let parentX = parentPosition.left - parentPosition.height + parentPosition.width/5;
+            let shiftAmount = parentX - centerPoint;
+            offset = "left:" + shiftAmount.toString() + "px";
+        }else{ 
+            let middleIndex = ~~(childrenIds.length/2);
+            let middleChild = document.getElementById(childrenIds[middleIndex]);
+            let middleChildPosition = middleChild?.getBoundingClientRect();
 
-        // let first = document.getElementById(childrenIds[0]);
-        // let last = document.getElementById(childrenIds[childrenIds.length-1]);
-        // if(!first || !last)
-        //     return;
-        // let firstPos = first.getBoundingClientRect();
-        // let lastPos = last.getBoundingClientRect();
-        // let fullLength = (lastPos.x + lastPos.width) - (firstPos.x + firstPos.width);
-        // let commonOffset = fullLength/(childrenIds.length-1);
-        // if(childrenIds.length % 2 === 0){
-        //     for(let i = 0; i < childrenIds.length; i++){
-        //         let child = document.getElementById(childrenIds[i]);
-        //         if(!child) {
-        //             continue;
-        //         }
-        //         let childPosition = child.getBoundingClientRect();
-        //         let center = parentPosition.x - childPosition.x;
-        //         if(i <= childrenIds.length/2){
-        //             let offset = "left:" + (center - (commonOffset * (childrenIds.length/2 - i))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         } else {
-        //             let offset = "left:" + (center + (commonOffset * ((i+1) - childrenIds.length/2))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         }
-        //     }
-        // } else {
-        //     for(let i = 0; i < childrenIds.length; i++){
-        //         let child = document.getElementById(childrenIds[i]);
-        //         if(!child) {
-        //             continue;
-        //         }
-        //         let childPosition = child.getBoundingClientRect();
-        //         let center = parentPosition.x - childPosition.x;
-        //         let middleIndex = ~~(childrenIds.length/2);
-        //         if(i === middleIndex){
-        //             let offset = "left:" + center.toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         }else if(i < middleIndex){
-        //             let offset = "left:" + (center - (commonOffset * (middleIndex - i))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         } else {
-        //             let offset = "left:" + (center + (commonOffset * (i - middleIndex))).toString() + "px";
-        //             child.setAttribute("style", offset);
-        //         }
-        //     }
-        // }
+            if(!middleChild || !middleChildPosition)
+                return;
+
+            let parentX = parentPosition.left - parentPosition.height + parentPosition.width/5;
+            let childX = middleChildPosition.left - middleChildPosition.height + middleChildPosition.width/5;
+            let shiftAmount = parentX - childX;
+            offset = "left:" + shiftAmount.toString() + "px";
+        }
+
+        for(let i = 0; i < childrenIds.length; i++){
+            let child = document.getElementById(childrenIds[i]);
+            if(!child) {
+                continue;
+            }
+            let childPosition = child.getBoundingClientRect();
+            child.setAttribute("style", offset);
+        }
     }
 
     drawLineConnection(parentId: string, childId: string, lineId1: string, lineId2: string, lineId3: string) {
@@ -157,17 +112,18 @@ export class HierarchicalViewComponent implements OnInit, AfterViewInit {
         let line3 = document.getElementById(lineId3);
         let childPos = child?.getBoundingClientRect();
         let parentPos = top?.getBoundingClientRect();
-        console.log(parentPos)
         if (!line1 || !line2 || !line3 || !childPos || !parentPos){
             console.log("drawing error");
             return;
         }
+        let parentEdge = parentPos.top + parentPos.height/6;
+        let childEdge = childPos.top - childPos.height/2;
+        let verticalLineLength = (childEdge-parentEdge)/2
 
-        let childVerticalLineLength = 30;
         let x1Offset = childPos.left - childPos.height + childPos.width/5;
-        let y1Offset = childPos.top - childPos.height/2;
+        let y1Offset = childEdge;
         let x2Offset = x1Offset;
-        let y2Offset = y1Offset - childVerticalLineLength;
+        let y2Offset = y1Offset - verticalLineLength;
         let connectionX1 = x2Offset;
         let connectionY1 = y2Offset; 
         let x1 = x1Offset.toString();
@@ -179,11 +135,10 @@ export class HierarchicalViewComponent implements OnInit, AfterViewInit {
         line2.setAttribute('x2', x1);
         line2.setAttribute('y2', y2);
 
-        let parentVerticalLineLength = 50;
         x1Offset = parentPos.left - parentPos.height + parentPos.width/5;
-        y1Offset = parentPos.top + parentPos.height/6; 
+        y1Offset = parentEdge; 
         x2Offset = x1Offset;
-        y2Offset = y1Offset + parentVerticalLineLength;
+        y2Offset = y1Offset + verticalLineLength;
         let connectionX2 = x2Offset;
         let connectionY2 = y2Offset; 
         x1 = x1Offset.toString();
