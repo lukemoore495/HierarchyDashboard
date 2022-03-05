c3395b7906df15993f0ffafbb12dfd50409994c3
mbozada <52613673+mbozada@users.noreply.github.com>
2022-03-04 19:34:27 -0500

    Initial POST with SimpleHierarchy.json
    POST works with SimpleHierarchy.json
    server.py is able to parse out the needed fields and add them to the database

--- backend/server.py
+++ backend/server.py
@@ -25,10 +25,31 @@ def _set_sqlite_pragma(dbapi_connection, connection_recor):
 db = SQLAlchemy(app)
 now = datetime.now()
 
+# Must be run everytime the database's structure is changed.
+# from server import db
+# db.create_all()
+# exit()
+
 # models
 class Hierarchy(db.Model):
     __tablename__ = "hierarchy"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(200))
+
+class Node(db.Model):
+    __tablename__ = "node"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    weight = db.Column(db.Float)
+    hierarchy_id = db.Column(db.Integer, db.ForeignKey("hierarchy.id"), nullable=False)
+
+class Measurement(db.Model):
+    __tablename__ = "measurement"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    type = db.Column(db.String(50))
+    node_id = db.Column(db.Integer, db.ForeignKey("node.id"), nullable=False)
 
 # routes
 @app.route("/", methods=['GET'])
@@ -37,7 +58,41 @@ def hello_world():
 
 @app.route("/hierarchy", methods=['POST'])
 def create_hierarchy():
-    pass
+    # Get data from .json sent with request
+    data = request.get_json()
+    # Create Hierarchy
+    new_hierarchy = Hierarchy(
+        name=data["name"],
+        description=data["description"],
+    )
+    db.session.add(new_hierarchy)
+    db.session.commit() # hierarchy now has a unique id
+
+    # Parse and create Nodes
+    nodes = data["nodes"]
+    for node in nodes:
+        new_node = Node(
+            name=node["name"],
+            weight=node["weight"],
+            hierarchy_id=new_hierarchy.id
+        )
+        db.session.add(new_node)
+        db.session.commit() # node now has a unique id
+
+        # Parse and create Measurements
+        measurements = node["measurements"]
+        for measurement in measurements:
+            new_measurement=Measurement(
+                name=measurement["measurementName"],
+                type=measurement["measurementType"],
+                node_id=new_node.id
+            )
+            db.session.add(new_measurement)
+            db.session.commit() # measurement now has a unique id
+
+
+
+    return jsonify({"message": "Hierarchy Created"}, 200)
 
 @app.route("/hierarchy/ascending_id", methods=['GET'])
 def get_all_hierarchies_ascending():
