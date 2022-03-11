35d38dfe7184c363202c9dcda871b668b2012d7f
Michael Bozada <52613673+mbozada@users.noreply.github.com>
2022-03-10 23:47:44 -0500

    POST/GET/DELETE Refactor (#43)
    * DELETE Single Node and Measurement
    
    Able to DELETE singular nodes and measurements based off of
    node_id and measurement_id
    
    * Initial GET Refactor
    
    Changed the GET functions to methods.
    
    This feels both right and wrong, so I'll hold off on doing anymore.
    
    * POST Singular Node or Measurement
    
    We can now add nodes or measurements.
    However, the .json to POST's format differs slightly from
    the RRRHierarchy.json or SimpleHierarchy.json files
    
    * Added singular GET to all Classes
    
    Used in the delete routes
    
    * Luke's Recommendations
    
    Fixed HTTP Return code for POST
    POST now also returns the entire created hierarchy
    
    * Another Refactor
    
    Based off of more of Luke's recommendations.
    Changed the returned responses on most routes.
    Changed formatting of routes to be in line with REST
    
    * DELETE 404
    
    DELETE routes now return 404 if the thing to be deleted does not exist.
    There's almost definitely a way to reduce code duplication, but I
    haven't found it yet.
    
    * Added TODOs
    
    Added a couple reminders for later.
    Wanted to get the current refactors POST/GET/DELETE to Luke ASAP
    
    * Fixed GET node and GET Measurement
    
    Created to_dict methods for Nodes and Measurements
    
    * Separated Models into Files
    
    * Deleted server.py
    
    * Changed Hierarchy.get_dict() to hierarchy.to_dict()
    
    * Updated README.md
    
    * Added 404 for GET Ascending and Descending
    
    Now it will return 404 errors if no hierarchies have been created yet.
    
    * Fixed HTTP Codes
    
    Now it should give the correct codes on responses
    
    * Switched back to listing of Headers
    
    GET Ascending and Descending return a list of headers again.
    
    * Made get_list() a bit more readable

---
+++ backend/app.py
@@ -0,0 +1,155 @@
+from flask import Flask, abort, jsonify, request
+from pathlib import Path
+
+from models.hierarchy import Hierarchy, Node, Measurement
+from models.shared import db
+
+
+# app
+app = Flask(__name__)
+
+# app configurations
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+app.config["JSON_SORT_KEYS"] = False
+
+db.init_app(app)
+
+
+# routes
+# TODO Add checks that hierarchies/nodes exist before adding subordinate resources
+@app.route("/hierarchy", methods=['POST'])
+def create_hierarchy():
+    # Get data from .json sent with request
+    data = request.get_json()
+
+    # Create Hierarchy
+    new_hierarchy = Hierarchy.create(data)
+
+    # Parse and create Nodes
+    nodes_lst = data["nodes"]
+    Node.create_nodes(nodes_lst, new_hierarchy.id)
+
+    return jsonify(new_hierarchy.to_dict()), 201
+
+
+# To add a node to the root of the hierarchy, send 0 for parent_id
+@app.route("/hierarchy/<hierarchy_id>/node/<parent_id>", methods=['POST'])
+def create_node(hierarchy_id, parent_id):
+    data = request.get_json()
+
+    if parent_id == 0:
+        parent_id = None
+
+    if "icon" in data:
+        icon = data["icon"]
+    else:
+        icon = None
+
+    new_node = Node.create(
+        data,
+        hierarchy_id=hierarchy_id,
+        parent_id=parent_id,
+        icon=icon
+    )
+
+    return jsonify(new_node.to_dict()), 201
+
+
+@app.route("/hierarchy/<hierarchy_id>/node/<node_id>/measurement", methods=['POST'])
+def create_measurement(hierarchy_id, node_id):
+    data = request.get_json()
+
+    new_measurement = Measurement.create(
+        data,
+        hierarchy_id=hierarchy_id,
+        node_id=node_id,
+    )
+
+    return jsonify(new_measurement.to_dict()), 201
+
+
+@app.route("/hierarchy/ascending_id", methods=['GET'])
+def get_all_hierarchies_ascending():
+    all_hierarchies = Hierarchy.get_list(get_nodes=False)
+
+    if not all_hierarchies:
+        abort(404, description="Resource not found")
+
+    return jsonify(all_hierarchies), 200
+
+
+@app.route("/hierarchy/descending_id", methods=['GET'])
+def get_all_hierarchies_descending():
+    all_hierarchies = Hierarchy.get_list(get_nodes=False)
+    all_hierarchies.reverse()
+
+    if not all_hierarchies:
+        abort(404, description="Resource not found")
+
+    return jsonify(all_hierarchies), 200
+
+
+@app.route("/hierarchy/<hierarchy_id>", methods=['GET'])
+def get_one_hierarchy(hierarchy_id):
+    hierarchy = Hierarchy.get(hierarchy_id)
+    
+    if not hierarchy:
+        abort(404, description="Resource not found")
+
+    return jsonify(hierarchy.to_dict()), 200
+
+
+# TODO Reduce the code duplication in the DELETE Routes
+@app.route("/hierarchy/<hierarchy_id>", methods=['DELETE'])
+def delete_hierarchy(hierarchy_id):
+    hierarchy = Hierarchy.get(hierarchy_id)
+
+    # hierarchy does not exist, return 404
+    if not hierarchy:
+        abort(404, description="Resource not found")
+
+    db.session.delete(hierarchy)
+    db.session.commit()
+
+    message = f"Hierarchy {hierarchy_id} Deleted"
+    return jsonify({"message": message}), 200
+
+
+@app.route("/node/<node_id>", methods=['DELETE'])
+def delete_node(node_id):
+    node = Node.get(node_id)
+
+    # node does not exist, return 404
+    if not node:
+        abort(404, description="Resource not found")
+
+    db.session.delete(node)
+    db.session.commit()
+
+    message = f"Node {node_id} Deleted"
+    return jsonify({"message": message}), 200
+
+
+@app.route("/measurement/<measurement_id>", methods=['DELETE'])
+def delete_measurement(measurement_id):
+    measurement = Measurement.get(measurement_id)
+
+    # measurement does not exist, return 404
+    if not measurement:
+        abort(404, description="Resource not found")
+
+    db.session.delete(measurement)
+    db.session.commit()
+
+    message = f"Measurement {measurement_id} Deleted"
+    return jsonify({"message": message}), 200
+
+if __name__ == "__main__":
+    # Create the database if it doesn't exist
+    path = Path("./app.db")
+    if not path.is_file():
+        with app.app_context():
+            db.create_all()
+
+    app.run(debug=True)
---
+++ backend/models/hierarchy.py
@@ -0,0 +1,49 @@
+from .shared import db
+from .node import Node, Measurement
+
+class Hierarchy(db.Model):
+    __tablename__ = "hierarchy"
+    id = db.Column(db.Integer, primary_key=True)
+    
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(200))
+    nodes = db.relationship("Node", cascade="all, delete")
+
+    @classmethod
+    def create(cls, hierarchy_data):
+        new_hierarchy = Hierarchy(
+            name=hierarchy_data["name"],
+            description=hierarchy_data["description"],
+        )
+
+        db.session.add(new_hierarchy)
+        db.session.commit() # hierarchy now has a unique id
+
+        return new_hierarchy
+    
+    @classmethod
+    def get(cls, hierarchy_id):
+        return Hierarchy.query.filter_by(id=hierarchy_id).first()
+    
+    def to_dict(self, get_nodes=True):
+        hier_dict = {
+            "id": str(self.id),
+            "name": self.name,
+            "description": self.description,
+        }
+
+        # Parse nodes and add list of dicts to hier_dict
+        if get_nodes:
+            hier_dict["nodes"] = Node.get_list(self.id, None)
+
+        return hier_dict
+    
+    @classmethod
+    def get_list(cls, get_nodes):
+        hierarchies = Hierarchy.query.all()
+        all_hierarchies = []
+
+        for hierarchy in hierarchies:
+            all_hierarchies.append(hierarchy.to_dict(get_nodes))
+        
+        return all_hierarchies
