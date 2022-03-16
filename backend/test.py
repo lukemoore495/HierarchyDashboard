505a03d6ac659b5df5614a75b219022b1cb3ebe3
mbozada <52613673+mbozada@users.noreply.github.com>
2022-03-16 17:36:03 -0400

    Added Resources
    Added a few comments with links to help with the refactor

--- backend/test.py
+++ backend/test.py
@@ -6,6 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
+# Set up the Hierarchy to point to the head node?
 class Hierarchy(db.Model):
     __tablename__ = 'hierarchy'
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +18,10 @@ class Hierarchy(db.Model):
         return '<Hiearchy %r>' % self.name
 
 
+# https://docs.sqlalchemy.org/en/14/orm/self_referential.html
+# https://docs.sqlalchemy.org/en/14/orm/examples.html#examples-adjacencylist
+# Use this to model the tree structure
+# Should measurements just be nodes with a measurement flag?
 class Node(db.Model):
     __tablename__ = 'node'
     id = db.Column(db.Integer, primary_key=True)
