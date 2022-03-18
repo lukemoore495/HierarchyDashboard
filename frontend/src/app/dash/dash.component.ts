f6c6bc3789e0ff53363eb0327de94eb0d5883e59
lukemoore495 <55766638+lukemoore495@users.noreply.github.com>
2022-03-18 16:53:37 -0400

    Prepopulate alternative form with existing values (#48)
    * Get prepopulation working the simple way
    
    * fix file errors
    
    * Fix boolean measure conversions

--- frontend/src/app/dash/dash.component.ts
+++ frontend/src/app/dash/dash.component.ts
@@ -4,7 +4,7 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { Store } from '@ngrx/store';
 import { HierarchyState } from '../state/hierarchy.reducer';
 import { getSelectedHierarchy } from '../state';
-import { Alternative } from '../hierarchy';
+import { Alternative } from '../Hierarchy';
 import { setSelectedAlternative, setSelectedHierarchy } from '../state/hierarchy.actions';
 import { MatSelectChange } from '@angular/material/select';
 import { Observable } from 'rxjs';
@@ -23,8 +23,8 @@ export class DashComponent{
     constructor(private breakpointObserver: BreakpointObserver, private store: Store<HierarchyState>) {
         this.alternatives$ = this.store.select(getSelectedHierarchy).pipe(
             map(hierarchy => hierarchy?.alternatives === undefined ? [] : hierarchy?.alternatives)
-        )
-     }
+        );
+    }
 
     /** Based on the screen size, switch from standard to one column per row */
     cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -47,5 +47,5 @@ export class DashComponent{
 
     onSelectionchange(event:MatSelectChange){
         this.store.dispatch(setSelectedAlternative({selectedAlternativeId: event.value}));
-      }
+    }
 }
