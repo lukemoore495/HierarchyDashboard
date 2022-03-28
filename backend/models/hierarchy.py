6f8d7d222175c14ba9755199133511a3b61e4f1f
mbozada <52613673+mbozada@users.noreply.github.com>
2022-03-27 20:07:56 -0400

    Initial hierarchy.py Documentation

--- backend/models/hierarchy.py
+++ backend/models/hierarchy.py
@@ -2,6 +2,31 @@ from .shared import db # Allows the models to be split out into separate files.
 from .node import Node
 
 class Hierarchy(db.Model):
+    """
+    A class used to generate a table of Nodes.
+
+    ...
+
+    Attributes
+    ----------
+    id : int
+        Unique identifer for the hierarchy.
+    name : str
+        The name of the hierarchy.
+    description : str
+        A description of the hierarchy.
+    nodes : Iterable
+        All nodes belonging to the hierarchy
+
+    Methods
+    -------
+    dump(_indent=0):
+        Returns a string representation of the hierarchy, including its hierarchy tree.
+    to_dict():
+        Returns a dictionary representation of the hierarchy and its tree.
+    get_list(get_nodes):
+        Returns a list of hierarchy objects. Flag determine if nodes are returned.
+    """
     __tablename__ = 'hierarchy'
     id = db.Column(db.Integer, primary_key=True)
     
@@ -16,17 +41,37 @@ class Hierarchy(db.Model):
         )
 
     def __init__(self, name, description):
+        """
+        Parameters
+        ----------
+        name : str
+            Name of the hierarchy to be created.
+        description : str
+            Description of the hierarchy to be created.
+
+        """
         self.name = name
         self.description=description
 
     def __repr__(self):
+        """Returns a string representation of a hierarchy, including nodes."""
         return f'Hierarchy: {self.id}, Name: {self.name}, Nodes: {self.nodes}'
 
     # Assumes the root node is the first node in the list
     def dump(self):
+        """Returns a formatted string representation of a hierarchy, including nodes."""
         return repr(self) + "\n\nTree:\n" + self.nodes[0].dump()
 
     def to_dict(self, get_nodes=True):
+        """
+        Returns a dictionary representation of the hierarchy and its tree.
+        Formats the fields according to the Frontend's requirements.
+    
+        Returns
+        -------
+        hier_dict : Dictionary
+            Dictionary representation of the hierarchy
+        """
         hier_dict = {
             "id": str(self.id),
             "name": self.name,
@@ -42,6 +87,20 @@ class Hierarchy(db.Model):
     
     @classmethod
     def get_list(cls, get_nodes):
+        """
+        Returns a list of hierarchy objects.
+        Flag determine if nodes are returned.
+        
+        Parameters
+        ----------
+        get_nodes : bool
+            Flag to deterimine of nodes are returned or not.
+        
+        Returns
+        -------
+        all_hierarchies : list
+            A list of all hierarchies as dicts.
+        """
         hierarchies = Hierarchy.query.all()
         all_hierarchies = []
 
