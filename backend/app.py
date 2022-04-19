8e179d4ebbe49de3645b7d9d7a567e20a5114b32
mbozada <52613673+mbozada@users.noreply.github.com>
2022-04-19 11:12:50 -0400

    Fixed list index out of range

--- backend/app.py
+++ backend/app.py
@@ -213,10 +213,12 @@ def patch_node(hierarchy_id, node_id):
         if node.vf_type == "Linear":
             if 'referencePoints' in m_data:
                 ref_data = m_data['referencePoints']
-                refs = [ref for ref in node.references]
-                for i in range(2):
-                    refs[i].x = ref_data[i]['x']
-                    refs[i].y = ref_data[i]['y']
+                
+                i = 0
+                for ref in node.references:
+                    ref.x = ref_data[i]['x']
+                    ref.y = ref_data[i]['y']
+                    i += 1
 
     # Root node has no parent, but it should still refresh weights.
     if parent:
